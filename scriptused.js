971630d9a2ec7cbb2f90df0f40f5cffd71342111
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2024-08-27 10:22:36 +0530

    Add files via upload

---
+++ scriptused.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+<!-- Setup and start animation! usinf typed js-->
+<script>
+    var typed = new Typed('#element', {
+        strings: ['Full stack Developer', 'Graphic Designer'],
+        typeSpeed: 50,
+    });
+</script>
